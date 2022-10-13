d026852f6871bc50003127125d449d9f83cc36f8
Mahijith <menon.mahijith@gmail.com>
2022-10-14 04:41:52 +0530

    added dictionary to the frontend

--- src/App.js
+++ src/App.js
@@ -7,28 +7,43 @@ var emojiDictionary={
   "😏":"Whats Up Smile",
   "😊":"Smile without Teeth",
   "🐶":"Dog",
-  "😒":"Staring"
+  "😒":"Staring",
+  "🔎":"Search",
+  "🏴‍☠️":"Pirate",
+  "🥰":"Love",
+  "🍌":"Banana"
 }
 function App() {
   const [meaning, setMeaning]= useState("");
 
+  const emojis=Object.keys(emojiDictionary);
+  console.log(emojis);
   function emojiInputHandler(event){
     var userInput = event.target.value;
     var meaning = emojiDictionary[userInput];
-    var n=Object.keys(emojiDictionary);
     if (meaning=== undefined){
       meaning = "We dont have this in our database"
-      {
-      <span>emojiDictionary.value</span>
-    }
     }
     setMeaning(meaning);
+  }
+  function emojiHandler(emoji){
+ var meaning=emojiDictionary[emoji];
+ setMeaning(meaning);
   }
   return(
      <div className="App">
       <h1>Inside Outt!</h1>
       <input onChange={emojiInputHandler}></input>
       <h2>{meaning}</h2>
+      <h3>Emoji we Know</h3>
+      {emojis.map(function(emoji) {
+        
+        return<span
+        key={emoji}
+        style={{fontSize:"2rem", padding:"0.5rem 1rem",cursor:'pointer'}}
+        onClick={()=>emojiHandler(emoji)}
+        >{emoji}</span>
+      }) }
      </div>
   )
 }
